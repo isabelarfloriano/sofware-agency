08884957c094c1d54a42f316287bb2aee1a4a349
Isabela Rozani Floriano <irf161295@gmail.com>
2022-07-29 19:33:59 -0300

    feat: graphic circle 'A' structure using Progressbar library

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 $(document).ready( function() {
   
+  let containerA = document.getElementById("circleA");
+
+  let circleA = new ProgressBar.Circle(containerA, {
+
+    color: '#65DAF9',
+    strokeWidth: 8,
+    duration: 1400,
+    from: { color: '#aaa'},
+    to: { color: '#65DAF9'},
+
+    step: function(state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+
+      var value = Math.round(circle.value() * 60);
+      circle.setText(value);
+
+    }
+
+  });
+  
 });
